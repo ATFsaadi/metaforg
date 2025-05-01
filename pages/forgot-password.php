bb3c69f63896d59f6e64e0fec04d892120ccd55a
ATFsaadi <atef.saadi1308@gmail.com>
2025-05-01 23:23:39 +0200

    structure projet

--- pages/forgot-password.php
+++ pages/forgot-password.php
@@ -3,7 +3,17 @@ session_start();
 
 include "../includes/header-PG.php";
 ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 
 
 
